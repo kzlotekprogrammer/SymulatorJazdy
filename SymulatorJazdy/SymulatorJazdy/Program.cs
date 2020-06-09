0bc1fc82827961b5ea2361db702e7b4168e9bdf0
kzlotekprogrammer <Extended1234>
2020-06-09 13:29:28 +0200

    Hello World

---
+++ SymulatorJazdy/SymulatorJazdy/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using SFML;
+using SFML.Graphics;
+using SFML.Window;
+
+namespace SymulatorJazdy
+{
+    class Program
+    {
+        private static RenderWindow renderWindow;
+        static void Main(string[] args)
+        {
+            renderWindow = new RenderWindow(new VideoMode(200, 200), "Hello world!");
+
+            CircleShape circleShape = new CircleShape(100.0f);
+            circleShape.FillColor = Color.Green;
+
+            renderWindow.Closed += RenderWindow_Closed;
+
+            while (renderWindow.IsOpen)
+            {
+                renderWindow.DispatchEvents();
+
+                renderWindow.Clear();
+                renderWindow.Draw(circleShape);
+                renderWindow.Display();
+            }
+        }
+
+        private static void RenderWindow_Closed(object sender, EventArgs e)
+        {
+            renderWindow.Close();
+        }
+    }
+}
